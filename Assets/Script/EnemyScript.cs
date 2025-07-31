996bb53fa2995e7165721b6559e02a869ccdcbe7
01254016 <ca01254016@st.kawahara.ac.jp>
2025-07-31 15:53:15 +0900

    <Scriot>Enemy追加

---
+++ Assets/Script/EnemyScript.cs
@@ -0,0 +1,105 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyScript : MonoBehaviour
+{
+    Vector2 startPosition = Vector2.zero;
+    float moveSpeed = 0.0f;
+    float downs = 0.0f;
+    float ups= 0.0f;
+    float genkaiy = 403f;
+    float genkaix = 680f;
+    int myStatus = 0;
+
+    bool frag = false;
+
+    float EnemyHp = 100.0f;
+    float EnemyAtk = 100.0f; 
+    void Start()
+    {
+        Camera.main.transform.position = startPosition;
+        startPosition -= Vector2.down * 1.5f*Time.deltaTime;
+      
+        moveSpeed = 2.5f;
+        downs = -6.5f;
+        ups = 6.5f;
+    }
+    void Update()
+    {        
+        float time = Time.deltaTime * 60;
+        //3•b‚ğ‰ß‚¬‚½‚ç‘¬“x‚ğ•Ï‚¦‚é
+        if (time > 3.0f)
+        {
+            moveSpeed = moveSpeed * 2;
+        }
+        else
+        {
+            moveSpeed = moveSpeed * 1;
+        }
+           //myStatus‚É‚æ‚Á‚Ä”»’f•ªŠò
+            switch (myStatus)
+            
+            {
+                case 0:
+
+                    break;
+
+                case 1:
+
+                    break;
+
+                case 2:
+
+                    break;
+
+                default:
+
+                    break;
+            }
+        InitCube();
+        MoveCube();
+    }
+
+    //Cube‚ğ‰Šúİ’è‚·‚é
+
+    void InitCube()
+    {
+        transform.position = startPosition;
+    }
+
+    //Cube‚ª“™‘¬ˆÚ“®‚·‚é
+    void MoveCube()
+    {
+        if (transform.position.x <= -100)
+        {
+            if (frag == false)
+            {
+                transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+                transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+
+                //ˆê’èã‚Ì‹——£‚ği‚ñ‚Å‚¢‚½‚ç
+                if (transform.position.y <= downs)
+                {
+                    frag = true;
+                }
+            }
+            else
+            {
+                transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+                transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+                if (transform.position.y >= ups)
+                {
+                    frag = false;
+                }
+            }
+            if (transform.position.x<=-403)
+            {
+                Destroy(gameObject);//©g‚ğÁ‚·
+            }
+        }
+    }
+}
