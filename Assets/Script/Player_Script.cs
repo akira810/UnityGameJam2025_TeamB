07b30f58002d4a79bb54a82e0431636d1b381675
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-07-31 09:51:53 +0900

    <Player_Script><Player_Prefab>追加

---
+++ Assets/Script/Player_Script.cs
@@ -0,0 +1,36 @@
+//
+//Player‚Ì“®‚«‚ğ§Œä‚·‚éƒNƒ‰ƒX
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+    
+    float horizontal = 0.0f;
+    float moveSpeed = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitCube()
+    {
+        
+        horizontal = 0.0f;
+        moveSpeed = 3.0f;
+    }
+
+    public void MoveCube()
+    {
+        
+    }
+}
