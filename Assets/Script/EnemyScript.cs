01ad81c66f7c2767fd30b358d8600292c024244b
01254016 <ca01254016@st.kawahara.ac.jp>
2025-08-01 15:52:48 +0900

    <Stage>敵移動制御スクリプト追加

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -6,22 +6,22 @@ using UnityEngine.UIElements;
 
 public class EnemyScript : MonoBehaviour
 {
-    Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0))
 
-    float EnemyHp = 100.0f;
-    float EnemyAtk = 100.0f;
     void Start()
     {
+        Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5, 10));
+       transform.position = Vector2.(0,0);
+        float EnemyHp = 100.0f;
+        float EnemyAtk = 100.0f;
         transform.position = Vector2();
     }
     void Update()
     {
-   
 
-
+        MoveEnemy();
+        ManageState();
     }  
 
-    //Cube‚ª“™‘¬ˆÚ“®‚·‚é
     void MoveEnemy()
     {
 
