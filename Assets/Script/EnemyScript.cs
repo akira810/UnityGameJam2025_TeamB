b26a9f1452605bccdb5fd5db23ce0ce3489140aa
01254016 <ca01254016@st.kawahara.ac.jp>
2025-08-01 13:56:01 +0900

    <Enemy>Scriptの調節

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -6,80 +6,35 @@ using UnityEngine.UIElements;
 
 public class EnemyScript : MonoBehaviour
 {
-    Vector2 startPosition = Vector2.zero;
-    float moveSpeed = 0.0f;
-    float downs = -400.0f;
-    float ups = 400.0f;
-    float genkaiy = -670f;
-    float genkaix = 670f;
-
-    bool frag = false;
+    Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0))
 
     float EnemyHp = 100.0f;
     float EnemyAtk = 100.0f;
     void Start()
     {
-        Camera.main.transform.position = startPosition;
-        startPosition -= Vector2.down * 1.5f * Time.deltaTime;
-
-        moveSpeed = 2.5f;
-        downs = -6.5f;
-        ups = 6.5f;
+        transform.position = Vector2();
     }
     void Update()
     {
-        float time = Time.deltaTime * 60;
-        //3•b‚ğ‰ß‚¬‚½‚ç‘¬“x‚ğ•Ï‚¦‚é
-        if (time > 3.0f)
-        {
-            moveSpeed = moveSpeed * 2;
-        }
-        else
-        {
-            moveSpeed = moveSpeed * 1;
-        }
-        InitCube();
-        MoveCube();
-    }
+   
 
-    //Cube‚ğ‰Šúİ’è‚·‚é
 
-    void InitCube()
-    {
-        transform.position = startPosition;
-    }
+    }  
 
     //Cube‚ª“™‘¬ˆÚ“®‚·‚é
-    void MoveCube()
+    void MoveEnemy()
     {
-        float time = Time.deltaTime * 60;
-        if (transform.position.x <= -100)
+
+    }
+
+    void ManageState()
+    {
+        switch(EnemyState)
         {
-            if (frag == false)
-            {
-                transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
-                transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+            case 0:
 
-                //ˆê’èã‚Ì‹——£‚ği‚ñ‚Å‚¢‚½‚ç
-                if (transform.position.y <= downs)
-                {
-                    frag = true;
-                }
-            }
-            else
-            {
-                transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
-                transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
 
-                if (transform.position.y >= ups)//
-                {
-                    frag = false;
-                }
-            }
-            if (time >= 6)//•b”‚ª6•bˆÈã‚½‚Á‚½‚ç
-            {
-                Destroy(gameObject);//©g‚ğÁ‚·
-            }
+
         }
     }
 }
