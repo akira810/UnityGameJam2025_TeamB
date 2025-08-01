a3400818837f2695e3b22bcd3f02fe236f7e7939
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-08-01 14:19:57 +0900

    <Player_NormalBullet>弾のループ追加

--- Assets/Script/Player_NormalBullet.cs
+++ Assets/Script/Player_NormalBullet.cs
@@ -7,26 +7,37 @@ public class Player_NormalBullet : MonoBehaviour
     [SerializeField] GameObject newBullet;
 
     Vector3 moveSpeed = Vector3.zero;
-
+    bool move = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitPNormalBullet();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            move = true;
+        }
+
+        if (move == true)
+        {
+            MovePNormalBullet();
+        }
     }
     public void InitPNormalBullet()
     {
         moveSpeed.x = 10.0f;
-        moveSpeed.y = 10.0f;
     }
 
     public void MovePNormalBullet()
-    {
-
+    {        
+        transform.position += moveSpeed;
+        if (transform.position.x > 680.0f)
+        {
+            transform.Translate(Vector3.zero);
+        }
     }
 }
