29af41068700b5669e711645264e1e4155bfb5b7
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-08-04 10:10:39 +0900

    <EnemyScript>修正

--- Assets/Script/EnemyScript.cs
+++ Assets/Script/EnemyScript.cs
@@ -9,17 +9,11 @@ public class EnemyScript : MonoBehaviour
 
     void Start()
     {
-        Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5, 10));
-       transform.position = Vector2.(0,0);
-        float EnemyHp = 100.0f;
-        float EnemyAtk = 100.0f;
-        transform.position = Vector2();
+        
     }
     void Update()
     {
 
-        MoveEnemy();
-        ManageState();
     }  
 
     void MoveEnemy()
@@ -29,12 +23,6 @@ public class EnemyScript : MonoBehaviour
 
     void ManageState()
     {
-        switch(EnemyState)
-        {
-            case 0:
-
-
-
-        }
+       
     }
 }
