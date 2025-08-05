e3edaebc92e2028da28c363b7df9a31f8b99b436
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-08-05 10:38:03 +0900

    NormalBulletをプレイヤーの位置にスポーンさせる

--- Assets/Script/NormalBullet_Script.cs
+++ Assets/Script/NormalBullet_Script.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class NormalBullet_Script : MonoBehaviour
 {
+    [SerializeField] GameObject newBullet;
+
+    [SerializeField] GameObject player;
+    Player_Script playerScript;
+
+    Vector3 firstPos;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerScript = GetComponent<Player_Script>();
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        firstPos = player.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Instantiate(newBullet , firstPos, transform.rotation);
+
+
+        }
     }
 }
